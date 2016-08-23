71ebaf0b129d49f054da4677444e45e3f3de7549
johnchower <johnchower@gmail.com>
2016-08-23 10:56:17 -0600

    Change default data location to looker_csvs subfolder.

--- fn_load_data.R
+++ fn_load_data.R
@@ -3,10 +3,10 @@
 # Gets data from the source and loads it into memory.
 
 load_data <- function(
-  data.loc = paste(getwd(), "input_csvs", sep = "/")
+  data.loc = paste(find_root("README.md"), "looker_csvs", sep = "/")
 ){
   data.loc %>%
-    dir %>%
+    paste(., dir(.), sep = "/") %>%
     as.list %>%
     lapply(FUN = read.csv)
 }
