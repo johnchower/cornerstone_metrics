66e8c0c226c1f70ac63b02c923d6a45a2cb07440
johnchower <johnchower@gmail.com>
2016-08-23 11:00:31 -0600

    Update scratch work

--- scratch_work.R
+++ scratch_work.R
@@ -1,7 +1,6 @@
-cvar <- c("2015-11-01","2015-11-02","All")
-nvar1 <- c(12,10,5)
-nvar2 <- c(7,5,6)
-data <- cbind.data.frame(cvar,nvar1,nvar2)
+install.packages("rprojroot")
+library(rprojroot)
 
-data %>%
-  rbind(list("add", sum(nvar1), sum(nvar2)))
+find_root("README.md")
+
+
