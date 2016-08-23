0e4b3ced04a868169db79f31c3db419353f65ce8
johnchower <johnchower@gmail.com>
2016-08-23 10:57:11 -0600

    Add user_platformaction_date.sql

---
+++ user_platformaction_date.sql
@@ -0,0 +1,6 @@
+/* SQL runner query for user_platformaction_date data set*/
+select distinct u.id, u.account_type, up.date_id
+from user_platform_action_facts up
+join user_dimensions u
+on u.id = up.user_id
+where u.email is not null 
