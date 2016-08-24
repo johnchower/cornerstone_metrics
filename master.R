5f45c43bea87909b10cdf48097709f143f31de39
johnchower <johnchower@gmail.com>
2016-08-24 13:38:08 -0600

    Add fn_calculate_back_weeks.r

--- master.R
+++ master.R
@@ -8,6 +8,7 @@ library(rprojroot)
 source('fn_calculate_WAU.r')
 source('fn_load_data.r')
 source('fn_rename_and_assign.r')
+source('fn_calculate_back_weeks.r')
 
 project_data_list <- load_data()
 
@@ -21,6 +22,8 @@ rm(project_data_list)
 source('one_off_create_champion_facts.r')
 champion.facts <- read.csv("champion_facts_final.csv", stringsAsFactors = F) %>%
   rename(champion_group = Label)
+
+
 
 
 
