49c37aee29989af5a90f2c7ac28f74eb31acccc1
johnchower <johnchower@gmail.com>
2016-08-24 13:44:27 -0600

    Format output as yyymmdd (numeric)

--- fn_calculate_back_weeks.R
+++ fn_calculate_back_weeks.R
@@ -2,11 +2,22 @@
 
 calculate_back_weeks <- 
   function(
-    rundate = Sys.Date()
-    , weeks_to_calc = c(1,2,4)
+    rundate = Sys.Date(),
+    weeks_to_calc = c(1,2,4)
   ){
-    startdates <- rundate - weeks_to_calc*7
-    enddate <- startdates + 6
+    startdates <- (rundate - weeks_to_calc*7) 
+    enddates <- (startdates + 6)
+    
+    startdates %<>%
+      as.character %>%
+      {gsub("-", "", .)} %>%
+      as.numeric
+    
+     enddates %<>%
+      as.character %>%
+      {gsub("-", "", .)} %>%
+      as.numeric
+    
     data.frame(
       week = weeks_to_calc,
       startdate = startdates,
