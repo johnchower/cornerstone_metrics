1cfbc5810ea43e2511b20a30095bd44aab27d8a6
johnchower <johnchower@gmail.com>
2016-08-24 15:58:18 -0600

    Allow user to set input_data_location parameter in preamble

--- calculate_WAU_metrics.r
+++ calculate_WAU_metrics.r
@@ -11,10 +11,11 @@ source('fn_load_data.r')
 source('fn_rename_and_assign.r')
 source('fn_calculate_back_weeks.r')
 
-run_date <- as.Date("2016-08-26")
+run_date <- as.Date("2016-08-19")
 weeks_to_measure <- c(1,2,4)
 remove_internal_users <- F
 save_location <- "~/Desktop"
+input_data_location <- paste(find_root("README.md"), "looker_csvs", sep = "/")
 save_name <- 
   paste(
     "WAU_metrics"
@@ -26,7 +27,7 @@ save_name <-
   )
 view_results <- T
 
-project_data_list <- load_data()
+project_data_list <- load_data(data.loc = input_data_location)
 
 project_data_list %>%
   lapply(
