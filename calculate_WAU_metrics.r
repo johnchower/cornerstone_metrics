fc3714c98245d636cd2b918b7fb6ce98db361d02
johnchower <johnchower@gmail.com>
2016-08-25 10:27:52 -0600

    Include some whitespace and comments for readability

--- calculate_WAU_metrics.r
+++ calculate_WAU_metrics.r
@@ -11,11 +11,18 @@ source('fn_load_data.r')
 source('fn_rename_and_assign.r')
 source('fn_calculate_back_weeks.r')
 
+# Parameters ####
+
 run_date <- as.Date("2016-08-19")
+
 weeks_to_measure <- c(1,2,4)
+
 remove_internal_users <- F
+
 save_location <- "~/Desktop"
+
 input_data_location <- paste(find_root("README.md"), "looker_csvs", sep = "/")
+
 save_name <- 
   paste(
     "WAU_metrics"
@@ -25,7 +32,9 @@ save_name <-
     , paste(weeks_to_measure, collapse = "")
     , sep = "_"
   )
-view_results <- T
+view_results <- T # If T, then the output csv will open automatically upon completion.
+
+# Calculation ####
 
 project_data_list <- load_data(data.loc = input_data_location)
 
